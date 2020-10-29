d015e03e3bb1cba37f324ef722c4e7ebf03dac57
Hydrataur <35537477+Hydrataur@users.noreply.github.com>
2020-10-29 17:54:54 +0200

    Added a message service to show action history

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero'
 import { HeroService } from "../hero.service";
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,21 +11,21 @@ import { HeroService } from "../hero.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
   heroes : Hero[];
   
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes(); 
   }
 
-  selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add('HeroesComponent: Selected hero is ' + hero.name);
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
