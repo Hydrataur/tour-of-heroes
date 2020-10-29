5ea0beaa3d7dcc60016a00234b4d2b5d4488fc56
Hydrataur <35537477+Hydrataur@users.noreply.github.com>
2020-10-29 16:45:58 +0200

    Displays list of selectable heroes

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Felosial Tinleaf'},
+    { id: 12, name: 'Byreth Gammidge'},
+    { id: 13, name: 'Halorem Sera'},
+    { id: 14, name: 'Ven'},
+    { id: 15, name: 'Helios Volstrea'},
+    { id: 16, name: 'Duo'},
+    { id: 17, name: 'Buvpoz Onima Onima Malcent'},
+    { id: 18, name: 'Reimund Klaffiele Filolial'},
+    { id: 19, name: 'Belleth Nuckelavee'},
+    { id: 20, name: 'Orleif Maryev'},
+    { id: 21, name: 'Snaga Oglokine'},
+    { id: 22, name: 'Chef'},
+    { id: 23, name: 'Sera'},
+    { id: 24, name: 'Khione Boreas'},
+    { id: 24, name: 'Gar D.N.'},
+    { id: 25, name: 'Lilt / Oriax Quest'},
+    { id: 26, name: 'Fi'},
+    { id: 27, name: 'Oscleipus Medusin'},
+    { id: 28, name: 'Chaograth Baedrin'},
+    { id: 29, name: 'Hylia'}
+]
